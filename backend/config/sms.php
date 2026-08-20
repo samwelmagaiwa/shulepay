e350d68a22e763f849e34ec93296a66ff6f92795
Samwel Magaiwa <samwelmagaiwa229@gmail.com>
2026-08-20 11:33:32 +0300

    fix: quote SMS_SENDER_ID value to handle whitespace in dotenv

--- backend/config/sms.php
+++ backend/config/sms.php
@@ -8,7 +8,7 @@ return [
         'api_url' => env('SMS_API_URL', 'https://messaging.kilakona.co.tz/api/v1/vendor/message/send'),
         'api_key' => env('SMS_API_KEY', ''),
         'secret_key' => env('SMS_SECRET_KEY', ''),
-        'sender_id' => env('SMS_SENDER_ID', 'MAGRETH SCH'),
+        'sender_id' => env('SMS_SENDER_ID', 'ShulePay'),
         'country_code' => env('SMS_COUNTRY_CODE', '255'),
     ],
 
